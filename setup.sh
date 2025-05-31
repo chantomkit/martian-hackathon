eb8fe5a91def92d5de8ced69052038957abd4e8b
Tom Chan <msctomchan@gmail.com>
2025-05-31 21:58:27 +0000

    env stuff

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
-uv venv
+uv venv --python 3.11
 
 source .venv/bin/activate
 
 uv pip install -r uv.lock
-
-uv pip install -r guardian-loop/requirements.txt
-
-uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
 
 python -m ipykernel install --user --name martian-hackathon --display-name "martian-hackathon"
